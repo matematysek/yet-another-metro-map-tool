fbc5ee12d8c8ad8d3e6e663579e5b133d8353d60
matematysek <gebauerm@gmail.com>
2023-06-25 18:07:34 +0200

    define more colors

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 const diau = start(554, -203)
-    .setColor('red')
+    .setColor('myred')
     .continue(['test', 'test', 'test'], 1414, 'ne', 'line-end')
 
 const verr = start(1050, 4)
-    .setColor('green')
+    .setColor('mygreen')
     .continue(['test', 'test'], 992, 's', 'line-end')
 
 const hord = start(54, 1000)
-    .setColor('yellow')
+    .setColor('myyellow')
     .continue(['test', 'test'], 992, 'e', 'line-end')
 
 const verl = start(50, 4)
-    .setColor('blue')
+    .setColor('myblue')
     .continue(['test', 'test'], 992, 's', 'line-end')
 
 const horu = start(54, 0)
-    .setColor('purple')
+    .setColor('mypurple')
     .continue(['test', 'test'], 992, 'e', 'line-end')
     
 const diad = start(554, -203)
-    .setColor('orange')
+    .setColor('myorange')
     .continue(['test', 'test', 'test'], 1406, 'se', 'line-end')
