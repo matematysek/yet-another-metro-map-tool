8c409ac19da351dc5c7f950e66e7b1e5076a81fa
matematysek <gebauerm@gmail.com>
2023-07-04 16:58:20 +0200

    F line done

--- praha2100.js
+++ praha2100.js
@@ -164,19 +164,15 @@ const B7 = start(1624, 600)
 
 const B8 = start(1524, 600)
     .setColor('linebyellow')
-    .continue(['Smíchovské nádraží', null], 46, 'e', 'line-end') 
+    .continue([' ', null], 46, 'e', 'line-end') 
 		
 const B9 = start(1474, 484)
     .setColor('linebyellow')
-    .continue([' ', ' '], 134, 'se', 'line-end')
+    .continue([' ', 'Smíchovské nádraží'], 134, 'se', 'line-end')
     
 const B10 = start(100, 500)
     .setColor('linebyellow')
     .continue(['Nádraží Nučice', 'Rudná', 'Nádraží Rudná', 'Chrášťany', 'Depo Zličín', 'Zličín', 'Stodůlky'], 200, 'e', 'line-end') 
-    
-const B11 = start(308, 500)
-    .setColor('linebyellow')
-    .continue([' ', 'Luka', 'Lužiny', 'Hůrka', 'Nové Butovice', 'Jinonice', 'Radlická'], 1108, 'e', 'line-end') 
       
 const D1 = start(100, 900)
     .setColor('linedblue')
@@ -293,8 +289,43 @@ const F5 = start(1824, 564)
 const F6 = start(1674, 600)
     .setColor('lineforange')
     .continue([null, ' '], 42, 'e', 'line-end')   
-		
+		  
+const F7 = start(308, 400)
+    .setColor('lineforange')
+    .continue(['Nádraží Řepy', 'Slánská'], 92, 'e', 'line-end') 
+    
+const F8 = start(358, 384)
+    .setColor('lineforange')
+    .continue([' ', ' '], 141, 'ne', 'line-end')
         
+const F9 = start(258, 484)
+    .setColor('lineforange')
+    .continue([' ', 'Jihozápadní Město'], 141, 'ne', 'line-end')
+        
+const F10 = start(408, 400)
+    .setColor('lineforange')
+    .continue([' ', 'Motol', 'Kotlářka', 'Košířská'], 1008, 'e', 'line-end') 
+    
+const F11 = start(1524, 364)
+    .setColor('lineforange')
+    .continue([' ', ' '], 272, 'se', 'line-end')
+    
+const F12 = start(2626, 1000)
+    .setColor('lineforange')
+    .continue([' ', 'Skalka'], 100, 'e', 'line-end')
+    
+const F13 = start(2782, 884)
+    .setColor('lineforange')
+    .continue(['Depo Hostivař', ' '], 141, 'ne', 'line-end') 
+    
+const F14 = start(2732, 1000)
+    .setColor('lineforange')
+    .continue([' ', 'Štěrbooholy', 'Slatiny', 'Dubeč'], 600, 'e', 'line-end')    
+    
+const B11 = start(308, 500)
+    .setColor('linebyellow')
+    .continue([' ', 'Luka', 'Lužiny', 'Hůrka', 'Nové Butovice', 'Jinonice', 'Radlická'], 1108, 'e', 'line-end') 
+		       
 const G1 = start(2478, 884)
     .setColor('linegsilver')
     .continue([' ', ' '], 141, 'ne', 'line-end')  
